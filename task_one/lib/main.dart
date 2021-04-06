fbe2880a4df9816b0f12fd1a2a2722a4b01325f7
nadahanii <66536961+nadahanii@users.noreply.github.com>
2021-04-06 20:52:30 +0200

    Update main.dart

--- task_one/lib/main.dart
+++ task_one/lib/main.dart
@@ -4,34 +4,41 @@ void main() {
   runApp(MyApp());
 }
 
-/* 
-during performing the task , i found that when i uncomment the border radius part ,
-this makes the border of the first image not visible.
- */
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          padding: EdgeInsets.only(
-              top: 150.0), //to separate the first image from the screen's top
+          margin: EdgeInsets.only(
+              top: 150.0, bottom: 145.0, right: 20.0, left: 20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+              bottomLeft: Radius.circular(20.0),
+              bottomRight: Radius.circular(20.0),
+            ),
+            border: Border.all(width: 4.0, color: Colors.black),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                blurRadius: 7.0,
+                spreadRadius: 5.0,
+                offset: Offset(7, 7),
+              ),
+            ],
+          ),
           child: Center(
             child: Column(
               children: [
                 Container(
                   //first image
-                  width: 400.0,
+                  width: 350.0,
                   height: 250.0,
                   decoration: BoxDecoration(
-                    //borderRadius:
-                    // BorderRadius.vertical(top: Radius.circular(16.0)),
-                    border: Border(
-                      top: BorderSide(color: Colors.red, width: 3.0),
-                      // bottom: BorderSide(color: Colors.red, width: 3.0),
-                      left: BorderSide(color: Colors.red, width: 3.0),
-                      right: BorderSide(color: Colors.red, width: 3.0),
-                    ),
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(16.0)),
                     image: DecorationImage(
                         image: AssetImage('assets/my_meme_1.jpg'),
                         fit: BoxFit.fill),
@@ -39,17 +46,11 @@ class MyApp extends StatelessWidget {
                 ),
                 Container(
                   //second image
-                  width: 400.0,
+                  width: 350.0,
                   height: 250.0,
                   decoration: BoxDecoration(
-                    // borderRadius:
-                    // BorderRadius.vertical(bottom: Radius.circular(16.0)),
-                    border: Border(
-                      bottom: BorderSide(color: Colors.red, width: 3.0),
-                      top: BorderSide(color: Colors.red, width: 3.0),
-                      left: BorderSide(color: Colors.red, width: 3.0),
-                      right: BorderSide(color: Colors.red, width: 3.0),
-                    ),
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(16.0)),
                     image: DecorationImage(
                         image: AssetImage('assets/my_meme_2.jpg'),
                         fit: BoxFit.fill),
