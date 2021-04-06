3a9f7857be67524b0e481123aa7176a7b1c6b2c0
nadahanii <66536961+nadahanii@users.noreply.github.com>
2021-04-06 19:08:33 +0200

    first commit

---
+++ task_one/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.only(
+              top: 150.0), //to separate the first image from the screen's top
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  //first image
+                  width: 400.0,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(16.0)),
+                    border: Border(
+                      top: BorderSide(color: Colors.red, width: 3.0),
+                      left: BorderSide(color: Colors.red, width: 3.0),
+                      right: BorderSide(color: Colors.red, width: 3.0),
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage('assets/my_meme_1.jpg'),
+                        fit: BoxFit.fill),
+                  ),
+                ),
+                Container(
+                  //second image
+                  width: 400.0,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(16.0)),
+                    border: Border(
+                      bottom: BorderSide(color: Colors.red, width: 3.0),
+                      top: BorderSide(color: Colors.red, width: 3.0),
+                      left: BorderSide(color: Colors.red, width: 3.0),
+                      right: BorderSide(color: Colors.red, width: 3.0),
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage('assets/my_meme_2.jpg'),
+                        fit: BoxFit.fill),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
